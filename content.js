401ed80f0194e5eb82416ef2fb3c4a169efb1aae
EvanFarris <evanfarris0@gmail.com>
2024-01-05 18:04:57 -0600

    Base functionality

---
+++ content.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded",removeElements())
+const observer = new MutationObserver(() => removeElements())
+observer.observe(document.getElementById("contents"),{childList: true, subtree: true})
+
+function removeElements() {
+	const pElement = document.getElementById("contents")
+	if(pElement){
+		const pContainers = pElement.getElementsByTagName("ytd-item-section-renderer")
+		if(pContainers){
+			removeTags(pContainers[pContainers.length - 1].children[2])
+		}
+	}
+
+}
+function removeTags(pElement) {
+	const tSet = new Set(["ytd-reel-shelf-renderer","ytd-shelf-renderer", "ytd-rich-section-renderer", "ytd-playlist-renderer", "ytd-radio-renderer"])
+	let pChildren = pElement.childNodes
+	
+	for (let i = pChildren.length - 1; i >= 0; i--){
+		if(tSet.has(pChildren[i].localName)) {
+			pChildren[i].remove()
+		}
+	}
+}
