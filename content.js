919e5189dd634e0e1c85e17ac770671b5c278859
EvanFarris <evanfarris0@gmail.com>
2024-01-06 00:47:37 -0600

    Further reduced bug chance. Ext. does not work when starting from YT homepage, only from a search result + refresh

--- content.js
+++ content.js
@@ -1,16 +1,49 @@
-document.addEventListener("DOMContentLoaded",startObservers())
-function startObservers() {
-	const observer = new MutationObserver(() => removeElements())
-	observer.observe(document.getElementById("contents"),{childList: true, subtree: true})
-	removeElements()
+document.addEventListener("DOMContentLoaded",startObservers(null,null))
+function startObservers(records, obs) {
+	let elObs = document.getElementById("contents")
+	if(elObs && (obs == null || obs.tag == "contents")){
+		console.log("contents observer started")
+		const observer = new MutationObserver(removeElements)
+		observer.observe(elObs,{childList: true, subtree: true})
+		if(obs != null){obs.disconnect()}
+		removeElements()
+	} else if (obs == null){
+		initialObserver("contents")
+	}
+	
+	let flexyObs = document.getElementsByTagName("ytd-watch-flexy")
+	if(flexyObs && flexyObs.length && (obs == null || obs.tag == "ytd-watch-flexy")){
+		console.log("Flexy observer started")
+		flexyObs = flexyObs[0]
+		const flexyObserver = new MutationObserver(removeElements)
+		flexyObserver.observe(flexyObs,{childList: true, subtree: true})
+		if(obs != null){obs.disconnect()}
+		removeElements()
+	} else if (obs == null){
+		initialObserver("ytd-watch-flexy")
+	}
 }
 
-function removeElements() {
+function initialObserver(tag) {
+	const observer = new MutationObserver(startObservers)
+	observer.tag = tag
+	let elObs = document.getElementById("content")
+	if(elObs){
+		console.log("Watcher for: " + tag + " started")
+		observer.observe(elObs, {childList: true, subtree: true})
+	}
+}
+function removeElements(records, obs) {
+	console.log("removeElements")
 	const pElement = document.getElementById("contents")
+	console.log(pElement)
 	if(pElement){
 		const pContainers = pElement.getElementsByTagName("ytd-item-section-renderer")
+		console.log(pContainers)
 		if(pContainers && pContainers.length){
+			//if(obs){obs.disconnect()}
 			removeTags(pContainers[pContainers.length - 1].children[2])
+			//if(obs){obs.observe(pElement,{childList: true, subtree: true})}
 		}
 	}
 
