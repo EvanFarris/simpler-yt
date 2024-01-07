698fad4e19cd6aeee9d45425125c0efbcc209db2
EvanFarris <evanfarris0@gmail.com>
2024-01-06 19:48:00 -0600

    Added more MutationObservers to ensure important observers get created. Now separately observes the ytd-search and ytd-browse branches of the DOM- can now start from homepage then search, instead of starting from a fresh search.

--- content.js
+++ content.js
@@ -1,57 +1,77 @@
-document.addEventListener("DOMContentLoaded",startObservers(null,null))
-function startObservers(records, obs) {
-	let elObs = document.getElementById("contents")
-	if(elObs && (obs == null || obs.tag == "contents")){
-		console.log("contents observer started")
-		const observer = new MutationObserver(removeElements)
-		observer.observe(elObs,{childList: true, subtree: true})
-		if(obs != null){obs.disconnect()}
-		removeElements()
-	} else if (obs == null){
-		initialObserver("contents")
-	}
-	
-	let flexyObs = document.getElementsByTagName("ytd-watch-flexy")
-	if(flexyObs && flexyObs.length && (obs == null || obs.tag == "ytd-watch-flexy")){
-		console.log("Flexy observer started")
-		flexyObs = flexyObs[0]
-		const flexyObserver = new MutationObserver(removeElements)
-		flexyObserver.observe(flexyObs,{childList: true, subtree: true})
-		if(obs != null){obs.disconnect()}
-		removeElements()
-	} else if (obs == null){
-		initialObserver("ytd-watch-flexy")
-	}
-}
+document.addEventListener("DOMContentLoaded",startPMObserver(null,null))
 
-function initialObserver(tag) {
-	const observer = new MutationObserver(startObservers)
+function startSuperObserver(tag) {
+	const observer = new MutationObserver(startPMObserver)
 	observer.tag = tag
 	let elObs = document.getElementById("content")
 	if(elObs){
-		console.log("Watcher for: " + tag + " started")
 		observer.observe(elObs, {childList: true, subtree: true})
 	}
 }
-function removeElements(records, obs) {
-	console.log("removeElements")
-	const pElement = document.getElementById("contents")
-	console.log(pElement)
-	if(pElement){
-		const pContainers = pElement.getElementsByTagName("ytd-item-section-renderer")
-		console.log(pContainers)
-		if(pContainers && pContainers.length){
-			//if(obs){obs.disconnect()}
-			removeTags(pContainers[pContainers.length - 1].children[2])
-			//if(obs){obs.observe(pElement,{childList: true, subtree: true})}
+
+function startPMObserver(r, obs) {
+	let pmo = document.getElementById("page-manager")
+	if(pmo && (obs == null || obs.tag == "page-manager")){
+		const observer = new MutationObserver(startSBObservers)
+		observer.observe(pmo,{childList: true})
+		if(obs != null){obs.disconnect()}
+		//maybe call determineWhichToRemoveFrom here?
+	} else if (obs == null){
+		startSuperObserver("page-manager")
+	}
+}
+
+function startSBObservers(r, obs) {
+	let searchElements = document.getElementsByTagName("ytd-search")
+	let browseElements = document.getElementsByTagName("ytd-browse")
+
+	if(searchElements.length && !obs.hasOwnProperty("sytsearch")) {
+		const searchContents = searchElements[0].querySelector("#contents")
+		if(searchContents){
+			const observer = new MutationObserver(searchCallback)
+			observer.observe(searchContents,{childList: true, subtree: true})
+			obs.sytsearch = true
 		}
+	}
+	if(browseElements.length && !obs.hasOwnProperty("sytbrowse")) {
+		const browseContents = browseElements[0].querySelector("#contents")
+		if(browseContents)
+		{
+			const observer = new MutationObserver(browseCallback)
+			observer.observe(browseContents,{childList: true, subtree: true})
+			obs.sytbrowse = true
+		}
+	}
+	
+	if(searchElements.length && browseElements.length){
+		obs.disconnect()
+	}
+}
+
+function searchCallback(r,obs) {
+	const sElement = document.getElementById("page-manager").getElementsByTagName("ytd-search")[0]
+	if(sElement.hasAttribute("role") && sElement.getAttribute("role") == "main") {
+		removeElements(sElement.querySelector("#contents"))
+	}
+}
+
+function browseCallback(r,obs) {
+	const bElement = document.getElementById("page-manager").getElementsByTagName("ytd-browse")[0]
+	if(bElement.hasAttribute("role") && bElement.getAttribute("role") == "main") {
+		removeElements(bElement.querySelector("#contents"))
+	}
+}
+
+function removeElements(contents) {
+	const pContainers = contents.getElementsByTagName("ytd-item-section-renderer")
+	if(pContainers && pContainers.length){
+		removeTags(pContainers[pContainers.length - 1].children[2])
 	}
 
 }
 function removeTags(pElement) {
 	const tSet = new Set(["ytd-reel-shelf-renderer","ytd-shelf-renderer", "ytd-rich-section-renderer", "ytd-playlist-renderer", "ytd-radio-renderer"])
 	let pChildren = pElement.childNodes
-	
 	for (let i = pChildren.length - 1; i >= 0; i--){
 		if(tSet.has(pChildren[i].localName)) {
 			pChildren[i].remove()
