b6586730f531db93c43687ccc7ab889aa52ecfd9
EvanFarris <evanfarris0@gmail.com>
2024-01-11 00:31:49 -0600

    Combined the separate observers/callbacks into one observer.callback. Now removes 'For You'/'Shorts' sections from channel and explore pages.

--- content.js
+++ content.js
@@ -1,7 +1,8 @@
-document.addEventListener("DOMContentLoaded",startPMObserver(null,null))
+document.addEventListener("DOMContentLoaded",startGenericObserver(null,null))
 
 function startSuperObserver(tag) {
-	const observer = new MutationObserver(startPMObserver)
+	//Function in line 5 was startPMObserver
+	const observer = new MutationObserver(startGenericObserver)
 	observer.tag = tag
 	let elObs = document.getElementById("content")
 	if(elObs){
@@ -9,105 +10,95 @@ function startSuperObserver(tag) {
 	}
 }
 
-function startPMObserver(r, obs) {
-	let pmo = document.getElementById("page-manager")
-	if(pmo && (obs == null || obs.tag == "page-manager")){
-		const observer = new MutationObserver(startSBObservers)
-		observer.observe(pmo,{childList: true})
+function startGenericObserver(r,obs) {
+	let pmEle = document.getElementById("page-manager")
+	if(pmEle){
+		const observer = new MutationObserver(generalCallback)
+		observer.observe(pmEle, {childList: true, attributes: true, subtree: true})
 		if(obs != null){obs.disconnect()}
-	} else if (obs == null){
-		startSuperObserver("page-manager")
-	}
+	} else if(obs == null){startSuperObserver("page-manager")}
 }
 
-function startSBObservers(r, obs) {
-	let searchElements = document.getElementsByTagName("ytd-search")
-	let browseElements = document.getElementsByTagName("ytd-browse")
-
-	if(searchElements.length && !obs.hasOwnProperty("sytsearch")) {
-		const searchContents = searchElements[0].querySelector("#contents")
-		if(searchContents){
-			const observer = new MutationObserver(searchCallback)
-			observer.observe(searchContents,{childList: true, subtree: true})
-			obs.sytsearch = true
-		}
-	}
-	if(browseElements.length && !obs.hasOwnProperty("sytbrowse")) {
-		const browseContents = browseElements[0].querySelector("#contents")
-		if(browseContents)
-		{
-			const observer = new MutationObserver(browseCallback)
-			observer.observe(browseContents,{childList: true, subtree: true})
-			obs.sytbrowse = true
-		}
-	}
-	
-	if(searchElements.length && browseElements.length){
-		obs.disconnect()
-	}
-}
-
-function searchCallback(r,obs) {
-	const sElement = document.getElementById("page-manager").getElementsByTagName("ytd-search")[0]
-	if(!obs.hasOwnProperty("sytsections") || obs.syttitle != document.title){
-		obs.sytsections = 1
-		obs.sytelements = 0
-		obs.syttitle = document.title
-		obs.newTitle = true
-		obs.isSearch = true
-	}
-	if(sElement.hasAttribute("role") && sElement.getAttribute("role") == "main") {
-		removeElements(sElement.querySelector("#contents"), obs)
-	}
-}
-
-function browseCallback(r,obs) {
-	const bElement = document.getElementById("page-manager").getElementsByTagName("ytd-browse")[0]
-	if(!obs.hasOwnProperty("sytsections")){
-		obs.sytsections = 1
-		obs.sytelements = 0
-		obs.syttitle = document.title
-		obs.newTitle = false
-		obs.isSearch = false
-	}
-	if(bElement.hasAttribute("role") && bElement.getAttribute("role") == "main") {
-		removeElements(bElement.querySelector("#contents"), obs)
-	}
-}
-
-function removeElements(contents, obs) {
-	let pContainers, elementsToCheck = []
-	if(obs.isSearch) {pContainers = contents.getElementsByTagName("ytd-item-section-renderer")}
-	else {pContainers = contents.childNodes}
-
-	if(obs.isSearch && pContainers && pContainers.length){
-		if(pContainers.length > obs.sytsections && obs.newTitle){return}
-		else if (pContainers.length > obs.sytsections) {
-			obs.sytsections = pContainers.length
+function initObs(obs, isSearch, title, subtype){
+	if(isSearch) {
+		if(!obs.hasOwnProperty("sytsections") || obs.syttitle != document.title || !obs.prevSearch){
+			obs.sytsections = 1
 			obs.sytelements = 0
-		} else {
-				obs.newTitle = false
+			obs.syttitle = title
+			obs.newTitle = true
 		}
-		elementsToCheck = pContainers[pContainers.length - 1].children[2].childNodes
+		obs.prevSearch = true
+	} else{
+		if(!obs.hasOwnProperty("map")){obs.map = new Map()}
+		if(!obs.map.has(subtype) || (obs.map.get(subtype))[0] != document.title){obs.map.set(subtype, [title, 0])}
 
-	} else if (!obs.isSearch){
-		elementsToCheck = pContainers
-	} else {return}
-	
-	if(elementsToCheck.length > obs.sytelements){
-		removeTags(elementsToCheck, obs)
+		obs.prevSearch = false
 	}
 }
 
-function removeTags(eList, obs) {
+function generalCallback(r,obs){
+	const mainElement = document.getElementById("page-manager").querySelector("ytd-browse[role='main'], ytd-search[role='main']")
+	if(!mainElement){return}
+	
+	const tag = mainElement.tagName.toLowerCase()
+	let isSearch, subtype = null
+	
+	if(tag == "ytd-search") {
+		initObs(obs, true, document.title)
+		isSearch = true
+	} else if (tag == "ytd-browse") {
+		subtype = mainElement.getAttribute("page-subtype")
+		initObs(obs, false, document.title, subtype)
+		isSearch = false
+	} else {return}
+	getElementsToCheck(mainElement.querySelector("#contents"), obs, isSearch, subtype=subtype)
+}
+
+function getElementsToCheck(contents, obs, isSearch, subtype) {
+	const exclusionSet = new Set(["history", "live", "news", "learning", "fashion"])
+	if(subtype && exclusionSet.has(subtype)){return}
+	
+	let elementsToCheck, prevElementLength
+	if (!isSearch){
+		elementsToCheck = contents.childNodes
+		prevElementLength = (obs.map.get(subtype))[1]
+	} else {
+		let pContainers = contents.getElementsByTagName("ytd-item-section-renderer")
+		if(pContainers && pContainers.length){
+			if(pContainers.length > obs.sytsections && obs.newTitle){return}
+			else if (pContainers.length > obs.sytsections) {
+				obs.sytsections = pContainers.length
+				obs.sytelements = 0
+			} else {obs.newTitle = false}
+		} else{
+			return}
+		elementsToCheck = pContainers[pContainers.length - 1].children[2].childNodes
+		prevElementLength = 0
+	}
+	if(elementsToCheck.length > prevElementLength) {
+		removeTags(elementsToCheck, obs, prevElementLength, isSearch, subtype)
+	}
+}
+
+function removeTags(eList, obs, prevLength, isSearch, subtype, title) {
 	const tSet = new Set(["ytd-reel-shelf-renderer","ytd-shelf-renderer", "ytd-rich-section-renderer", "ytd-playlist-renderer", "ytd-radio-renderer","ytd-horizontal-card-list-renderer", "ytd-movie-renderer"])
-	for (let i = eList.length - 1; i >= obs.sytelements; i--){
-		if(tSet.has(eList[i].tagName.toLowerCase())) {
-			eList[i].remove()
-		} else if(eList[i].tagName.toLowerCase() == "ytd-video-renderer") {
+	const cSet = new Set(['For You', 'Shorts'])
+	const stSet = new Set(["home", "subscriptions"])
+	
+	let curTag
+	//homepage regenerates shorts section
+	if(subtype && stSet.has(subtype)){prevLength = 0}
+	for (let i = eList.length - 1; i >= prevLength; i--){
+		curTag = eList[i].tagName.toLowerCase()
+		if(tSet.has(curTag)) {eList[i].remove()} 
+		else if(curTag == "ytd-video-renderer") {
 			videoLink = eList[i].getElementsByTagName("a")
 			if(videoLink.length && videoLink[0].getAttribute("href").match(/shorts/)){eList[i].remove()}
+		} else if(curTag == "ytd-item-section-renderer"){
+			spanExists = eList[i].querySelector("span#title")
+			if(spanExists && cSet.has(spanExists.innerText)){eList[i].remove()}
 		}
 	}
-	obs.sytelements = eList.length
+	if(isSearch){obs.sytelements = eList.length}
+	else {obs.map.set(subtype, [document.title, eList.length])}
 }
